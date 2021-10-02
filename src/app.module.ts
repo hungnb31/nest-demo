068b500009862cbd1ea45c47a732cd812f726246
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-03 00:22:00 +0700

    updated users and reports entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,21 @@ import { UsersController } from './users/users.controller';
 import { ReportsController } from './reports/reports.controller';
 import { UsersService } from './users/users.service';
 import { ReportsService } from './reports/reports.service';
+import { UsersModule } from './users/users.module';
+import { Users } from './users/users.entity';
+import { ReportsModule } from './reports/reports.module';
+import { Reports } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [Users, Reports],
       synchronize: true,
     }),
+    UsersModule,
+    ReportsModule,
   ],
   controllers: [AppController, UsersController, ReportsController],
   providers: [AppService, UsersService, ReportsService],
