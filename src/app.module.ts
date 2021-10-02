78d98383e8c17845e5947f49497deb2199e4ab8f
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-02 23:00:21 +0700

    init db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
@@ -7,7 +9,14 @@ import { UsersService } from './users/users.service';
 import { ReportsService } from './reports/reports.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, UsersController, ReportsController],
   providers: [AppService, UsersService, ReportsService],
 })
