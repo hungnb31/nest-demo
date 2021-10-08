5735a18c414d91733ffccf84cf02271213073ece
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-08 11:10:08 +0700

    updated some hooks for entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterUpdate,
+  AfterRemove,
+} from 'typeorm';
 
 @Entity()
 export class Users {
@@ -10,4 +17,23 @@ export class Users {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserting an user with id: ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updating an user with id: ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removing an user with id: ', this.id);
+  }
 }
+
+// NOTE:
+// Those hooks above can only execute when we create an instance
+// instead of passing object directly to method
