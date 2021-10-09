e101e7386ab9719cd9fd675c6ec568f608cf67e3
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-09 11:10:48 +0700

    auth: setup auth service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { Users } from './users.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
   exports: [TypeOrmModule],
 })
 export class UsersModule {}
