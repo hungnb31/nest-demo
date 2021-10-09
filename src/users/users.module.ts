a8496d528b3c99a6d94b8a090fa873ce4ba91e94
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-09 18:24:23 +0700

    auth: set current user interceptor to global, so every route in app get current user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,11 +6,19 @@ import { UsersService } from './users.service';
 import { Users } from './users.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      useClass: CurrentUserInterceptor,
+      provide: APP_INTERCEPTOR,
+    },
+  ],
   exports: [TypeOrmModule],
 })
 export class UsersModule {}
