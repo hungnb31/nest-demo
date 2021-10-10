72dd5361f59b0d869b1a705a2e1c21ada8492e68
Hung Nguyen Ba <hungnb31@gmail.com>
2021-10-10 21:27:04 +0700

    testing: test if email in use

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,13 +5,14 @@ import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // create a fake copy of user service
 
     // we just need to fake find and create method because we only use
     // those two methods in auth service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as Users),
@@ -40,5 +41,19 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'abc@mail.com', password: '123123' } as Users,
+      ]);
+
+    try {
+      await service.signup('abc@mail.com', '123123');
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error.message).toEqual('Email in use!');
+    }
   });
 });
